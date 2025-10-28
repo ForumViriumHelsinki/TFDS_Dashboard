1df83ba6dbae5c75f322e979eb7f4cffcf82242e
Valmetti <vmvalonen@gmail.com>
2025-10-28 22:52:09 +0200

    feat: enhance App layout with new data display feature and improve global styles for better responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
 import "./App.css";
-import { AppShell, Checkbox, Group, Tabs, Text } from '@mantine/core';
-import { Image } from '@mantine/core';
+import {
+  AppShell,
+  Box,
+  Button,
+  Checkbox,
+  Group,
+  Stack,
+  Tabs,
+  Text,
+} from "@mantine/core";
+import { Image } from "@mantine/core";
 import { DateTimePicker } from "@mantine/dates";
+import { useDisclosure } from "@mantine/hooks";
 
 function App() {
+  const [dataDisplayOpened, { toggle: toggleDataDisplay }] =
+    useDisclosure(false);
+
   return (
-    <AppShell header={{ height: 60 }} navbar={{ width: 300, breakpoint: 'sm' }}>
+    <AppShell
+      h="100vh"
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm" }}
+    >
       <AppShell.Header px="md" py="sm">
         <Group justify="space-between">
           <Group gap="lg">
-            <Image src="/images/logos/forumvirium-orange.svg" alt="Forum Virium Helsinki" w={74} />
-            <Image src="/images/logos/helsinki-black.svg" alt="Helsinki" w={66} />
-            <Image src="/images/logos/TFDS.svg" alt="Traffic and Floating Data Space" w={128} />
+            <Image
+              src="/images/logos/forumvirium-orange.svg"
+              alt="Forum Virium Helsinki"
+              w={74}
+            />
+            <Image
+              src="/images/logos/helsinki-black.svg"
+              alt="Helsinki"
+              w={66}
+            />
+            <Image
+              src="/images/logos/TFDS.svg"
+              alt="Traffic and Floating Data Space"
+              w={128}
+            />
           </Group>
           <Checkbox.Group>
             <Group>
@@ -23,33 +52,49 @@ function App() {
             </Group>
           </Checkbox.Group>
           <Group gap="lg">
-            <Image src="/images/logos/edpsm.webp" alt="European data space for smart communities" w={128} />
-            <Image src="/images/logos/co-fundedbytheEU.png" alt="Co-funded by the EU" w={144} />
+            <Image
+              src="/images/logos/edpsm.webp"
+              alt="European data space for smart communities"
+              w={128}
+            />
+            <Image
+              src="/images/logos/co-fundedbytheEU.png"
+              alt="Co-funded by the EU"
+              w={144}
+            />
           </Group>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p={0}>
-      <Tabs defaultValue="Häiriöt">
-      <Tabs.List grow>
-        <Tabs.Tab value="Häiriöt">
-          Häiriöt
-        </Tabs.Tab>
-        <Tabs.Tab value="Ilmanlaatu">
-          Ilmanlaatu
-        </Tabs.Tab>
-      </Tabs.List>
+        <Tabs defaultValue="Häiriöt">
+          <Tabs.List grow>
+            <Tabs.Tab value="Häiriöt">Häiriöt</Tabs.Tab>
+            <Tabs.Tab value="Ilmanlaatu">Ilmanlaatu</Tabs.Tab>
+          </Tabs.List>
 
-      <Tabs.Panel value="Häiriöt" p="md">
-        Häiriöt tab content
-      </Tabs.Panel>
+          <Tabs.Panel value="Häiriöt" p="md">
+            Häiriöt tab content
+          </Tabs.Panel>
 
-      <Tabs.Panel value="Ilmanlaatu" p="md">
-        Ilmanlaatu tab content
-      </Tabs.Panel>
-    </Tabs>
+          <Tabs.Panel value="Ilmanlaatu" p="md">
+            Ilmanlaatu tab content
+          </Tabs.Panel>
+        </Tabs>
       </AppShell.Navbar>
-      <AppShell.Main>
-        Main content
+      <AppShell.Main h="100%">
+        <Stack gap={0} h="100%" bg="red">
+          <Box bg="gray.1" p="md" flex={1} h="100%">
+            <Text>Map content</Text>
+          </Box>
+          <Box bg="white" p="md" flex={dataDisplayOpened ? 1 : 0} h="100%">
+            <Group justify="space-between">
+              <Text>Data display content</Text>
+              <Button size="xs" variant="white" onClick={toggleDataDisplay}>
+                {dataDisplayOpened ? "Sulje" : "Näytä"}
+              </Button>
+            </Group>
+          </Box>
+        </Stack>
       </AppShell.Main>
     </AppShell>
   );
