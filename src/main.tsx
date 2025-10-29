316702126cd38f9cfb5af8252f25481713eec10e
Valmetti <vmvalonen@gmail.com>
2025-10-29 10:29:28 +0200

    style: add Mantine dates styles for improved date picker styling in main application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 import "./index.css";
 import App from "./App";
 import { createTheme, MantineProvider } from "@mantine/core";
