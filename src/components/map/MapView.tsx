55446c760f658d602e974251c7e586950ee641c2
Valmetti <vmvalonen@gmail.com>
2025-10-31 12:21:01 +0200

    feat: integrate Leaflet for map visualization and update dependencies

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,15 +1,27 @@
-import { Box, Text } from "@mantine/core";
-import { useAtomValue } from "jotai";
-import { selectedSegmentAtom } from "../../atoms/segments";
+import { Box } from "@mantine/core";
+import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet";
 
 export function MapView() {
-  const selectedSegment = useAtomValue(selectedSegmentAtom);
-
   return (
-    <Box bg="gray.1" p="md" flex={1} h="100%">
-      <Text>Map content</Text>
-      {selectedSegment && <Text size="sm" c="dimmed">Selected: {selectedSegment}</Text>}
+    <Box bg="gray.1" flex={1} h="100%">
+      <div id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </Box>
   );
 }
-
