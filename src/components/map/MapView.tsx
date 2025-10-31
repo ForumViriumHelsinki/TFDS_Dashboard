a21210ec585a7df498029029e6be1d53f6f867dc
Valmetti <vmvalonen@gmail.com>
2025-10-31 12:36:22 +0200

    feat: update map component with WMS layers and adjust map settings for improved visualization

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,25 +1,53 @@
 import { Box } from "@mantine/core";
-import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Popup, Marker, TileLayer, LayersControl, WMSTileLayer } from "react-leaflet";
 
 export function MapView() {
   return (
     <Box bg="gray.1" flex={1} h="100%">
       <div id="map">
         <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
+          center={[60.1699, 24.9384]}
+          zoom={15}
           scrollWheelZoom={false}
           style={{ height: "100%", width: "100%" }}
         >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          <LayersControl position="topright">
+
+            <LayersControl.BaseLayer checked name="Taustakartta">
+              <WMSTileLayer
+                url="https://kartta.hel.fi/ws/geoserver/avoindata/wms?"
+                layers="avoindata:Opaskartta_PKS_harmaa"
+                format="image/png"
+                transparent={false}
+                attribution="<a href='https://kartta.hel.fi/avoindata'>Helsingin opaskartta &#169; Helsingin kaupunkiympäristön toimiala / Kaupunkimittauspalvelut</a>"
+              />
+            </LayersControl.BaseLayer>
+
+            <LayersControl.BaseLayer name="Ilmakuva">
+              <WMSTileLayer
+                url="https://kartta.hel.fi/ws/geoserver/avoindata/wms?"
+                layers="avoindata:Ortoilmakuva"
+                format="image/png"
+                transparent={false}
+                attribution="<a href='https://kartta.hel.fi/avoindata'>Helsingin ortoilmakuva &#169; Helsingin kaupunkiympäristön toimiala / Kaupunkimittauspalvelut</a>"
+              />
+            </LayersControl.BaseLayer>
+
+            <LayersControl.BaseLayer name="OpenStreetMap">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            
+            <LayersControl.Overlay name="Marker">
+              <Marker position={[60.1699, 24.9384]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </LayersControl.Overlay>
+          </LayersControl>
         </MapContainer>
       </div>
     </Box>
