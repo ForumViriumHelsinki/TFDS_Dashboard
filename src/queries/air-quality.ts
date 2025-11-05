94d273acb8965df183402812db821f1a30839964
Valmetti <vmvalonen@gmail.com>
2025-11-05 12:10:23 +0200

    refactor: remove Jotai atom for air quality state management and introduce query-based data fetching

---
+++ src/queries/air-quality.ts
@@ -0,0 +1,29 @@
+import { queryOptions } from "@tanstack/react-query";
+import { AirQualityProps } from "../utils/airQuality";
+import type { FeatureCollection, Geometry } from "geojson";
+
+export const AirQualityTypes = {
+  AIR_QUALITY_NOW: "Ilmanlaatu_nyt",
+  AIR_QUALITY_24H_MAX: "Ilmanlaatu_24h_maksimiarvo",
+} as const;
+// eslint-disable-next-line no-redeclare
+export type AirQualityTypes =
+  (typeof AirQualityTypes)[keyof typeof AirQualityTypes];
+
+export interface ListAirQualityRequest {
+  airQualityType: AirQualityTypes;
+}
+
+export const getListAirQualityQueryOptions = (
+  requestParams: ListAirQualityRequest
+) =>
+  queryOptions({
+    queryKey: ["air-quality", requestParams.airQualityType],
+    queryFn: async () => {
+      const response = await fetch(
+        `/hsy-wfs/geoserver/wfs?version=2.0.0&request=GetFeature&typeNames=${requestParams.airQualityType}&outputFormat=application/json&srsName=urn:ogc:def:crs:EPSG::4326`
+      );
+      const data = await response.json();
+      return data as FeatureCollection<Geometry, AirQualityProps>;
+    },
+  });
